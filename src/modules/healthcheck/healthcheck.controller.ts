baaee97879800a19b2c4511f451ae79aec58889c
Alex <alexpenedo@gmail.com>
2018-09-04 18:35:42 +0200

    hostname on healthcheck

--- src/modules/healthcheck/healthcheck.controller.ts
+++ src/modules/healthcheck/healthcheck.controller.ts
@@ -1,11 +1,12 @@
 import {Controller, Get} from "@nestjs/common";
 import {ApiUseTags} from "@nestjs/swagger";
+import * as os from "os";
 
 @ApiUseTags('Healthcheck')
 @Controller('status')
 export class HealthcheckController {
     @Get()
     async get() {
-        return "OK";
+        return "OK from " + os.hostname();
     }
 }
