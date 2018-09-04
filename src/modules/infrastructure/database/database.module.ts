4cf507586ccd60f38fddef2383075ee7d8813c71
Alex <alexpenedo@gmail.com>
2018-09-04 15:44:46 +0200

    Prueba mongo cluster

--- src/modules/infrastructure/database/database.module.ts
+++ src/modules/infrastructure/database/database.module.ts
@@ -4,10 +4,11 @@ import {MongooseModule} from "@nestjs/mongoose";
 
 export class DatabaseModule {
     static forRoot(config: ConfigService): DynamicModule {
-        const mongoHost = `${config.mongoHost}:${config.mongoPort}/${config.mongoSchema}`;
+        const mongoHost = `${config.mongoHost}/${config.mongoSchema}`;
+        const mongoOptions = {useNewUrlParser: true};
         return {
             module: DatabaseModule,
-            imports: [MongooseModule.forRoot(mongoHost)]
+            imports: [MongooseModule.forRoot(mongoHost, mongoOptions)]
         };
     }
 }
