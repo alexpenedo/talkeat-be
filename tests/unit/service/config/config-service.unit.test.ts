283c24faacb34f98b16c63e4aaebc05ecf3d28ae
Alex <alexpenedo@gmail.com>
2018-09-04 15:48:19 +0200

    Mongo port propery deleted

--- tests/unit/service/config/config-service.unit.test.ts
+++ tests/unit/service/config/config-service.unit.test.ts
@@ -31,7 +31,6 @@ describe('ConfigService Unit tests', () => {
             expect(configService.isDevelopmentEnv).toBe(false);
             expect(configService.port).toBe(3977);
             expect(configService.jwtSecret).toBe('test-secret');
-            expect(configService.mongoPort).toBe(27017);
             expect(configService.mongoSchema).toBe('test');
             expect(configService.mongoDebug).toBe(false);
             expect(configService.accessTokenExpires).toBe('1h');
